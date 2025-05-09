a499daebe4354cfe2eaaea7868444f16d847e0c4
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2025-05-09 19:28:22 -0300

    solve 2024_03 in kotlin

---
+++ 2024/2024_03_kotlin/src/2024_03_kotlin.kt
@@ -0,0 +1,40 @@
+import java.io.File
+
+fun readInput(): String {
+    return File("input.txt").readText().trim()
+}
+
+fun parseMul(mul: String): Int {
+    val numPattern = Regex("""\d{1,3}""")
+    val (num1, num2) = numPattern.findAll(mul).map { it.value.toInt() }.toList()
+    return num1 * num2
+}
+
+fun part1(input: String): Int {
+    val mulPattern = Regex("""mul\(\d{1,3},\d{1,3}\)""")
+    val muls = mulPattern.findAll(input).map { it.value }
+    return muls.sumOf { parseMul(it) }
+}
+
+fun part2(input: String): Int {
+    val instructionsPattern = Regex("""(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))""")
+    val instructions = instructionsPattern.findAll(input).map { it.value }.toList()
+
+    val enabledMuls = mutableListOf<String>()
+    var disabled = false
+    for (instruction in instructions) {
+        when (instruction) {
+            "don't()" -> disabled = true
+            "do()" -> disabled = false
+            else -> if(!disabled) enabledMuls.add(instruction)
+        }
+    }
+
+    return enabledMuls.sumOf { parseMul(it) }
+}
+
+fun main() {
+    val input = readInput()
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
