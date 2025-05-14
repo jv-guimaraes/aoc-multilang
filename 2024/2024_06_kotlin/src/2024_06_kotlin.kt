b53d640e23318e1b6a218bcb729a1d4c934561cc
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2025-05-14 12:30:53 -0300

    Solve 2024_06 part 2 in kotlin

--- 2024/2024_06_kotlin/src/2024_06_kotlin.kt
+++ 2024/2024_06_kotlin/src/2024_06_kotlin.kt
@@ -5,7 +5,11 @@ data class Vec(val x: Int, val y: Int) {
     fun turnRight() = Vec(-y, x)
 }
 
-data class Guard(var pos: Vec, var dir: Vec)
+data class Guard(var pos: Vec, var dir: Vec) {
+    fun turnRight() {
+        dir = dir.turnRight()
+    }
+}
 
 object Dirs {
     val UP = Vec(0, -1)
@@ -15,6 +19,43 @@ object Dirs {
 }
 
 typealias Grid = MutableList<CharArray>
+
+fun Grid.deepCopy(): Grid {
+    return this.map { charArray -> charArray.copyOf() }.toMutableList()
+}
+
+operator fun Grid.get(pos: Vec): Char = this[pos.y][pos.x]
+
+operator fun Grid.set(pos: Vec, char: Char) {
+    this[pos.y][pos.x] = char
+}
+
+fun Grid.inside(pos: Vec): Boolean {
+    if (pos.y !in this.indices) return false
+    return pos.x in this[pos.y].indices
+}
+
+fun Grid.findGuard(): Guard {
+    this.forEachIndexed { y, row ->
+        row.forEachIndexed { x, charInCell ->
+            if (charInCell in charArrayOf('^', 'v', '<', '>')) {
+                return Guard(Vec(x, y), charToDir(charInCell))
+            }
+        }
+    }
+    error("No Guard found in the map")
+}
+
+fun clearTerminalWindows() {
+    try {
+        val processBuilder = ProcessBuilder("cmd", "/c", "cls")
+        processBuilder.inheritIO()
+        val process = processBuilder.start()
+        process.waitFor()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
 
 fun charToDir(char: Char): Vec {
     return when (char) {
@@ -26,54 +67,58 @@ fun charToDir(char: Char): Vec {
     }
 }
 
-fun findInitialGuard(grid: Grid): Guard {
-    grid.forEachIndexed { y, row ->
-        row.forEachIndexed { x, charInCell ->
-            if (charInCell in setOf('^', 'v', '<', '>')) {
-                return Guard(Vec(x, y), charToDir(charInCell))
-            }
-        }
-    }
-    error("No Guard found in the map")
-}
-
-operator fun Grid.get(pos: Vec): Char = this[pos.y][pos.x]
-
-operator fun Grid.set(pos: Vec, char: Char) {
-    this[pos.y][pos.x] = char
-}
-
-fun Grid.isValid(pos: Vec): Boolean {
-    if (pos.y !in this.indices) return false
-    return pos.x in this[pos.y].indices
-}
-
-fun readInput(filename: String = "input.txt"): Grid {
+fun readInput(filename: String = "test_input.txt"): Grid {
     return File(filename).readLines().map { it.toCharArray() }.toMutableList()
 }
 
-fun part1(grid: Grid): Int {
-    val guard = findInitialGuard(grid)
+fun possibleWallPositions(guard: Guard, grid: Grid): Set<Vec> {
+    val guard = guard.copy()
+    val firstPosition = guard.pos.copy()
+    val tiles = mutableSetOf<Vec>()
 
     while (true) {
-        grid[guard.pos] = 'x'
-
+        tiles.add(guard.pos)
         val nextPos = guard.pos + guard.dir
-        if (!grid.isValid(nextPos)) {
-            break
-        }
 
-        if (grid[nextPos] == '#') {
-            guard.dir = guard.dir.turnRight()
-        } else {
-            guard.pos = nextPos
+        when {
+            !grid.inside(nextPos) -> return (tiles - firstPosition)
+            grid[nextPos] in charArrayOf('#', 'O') -> guard.turnRight()
+            else -> guard.pos = nextPos
         }
     }
+}
 
-    return grid.sumOf { row -> row.count { it == 'x' } }
+fun hasLoop(guard: Guard, grid: Grid): Boolean {
+    val guard = guard.copy()
+    val previousGuards = mutableSetOf<Guard>()
+
+    while (true) {
+        if (guard in previousGuards) return true
+        previousGuards.add(guard.copy())
+
+        val nextPos = guard.pos + guard.dir
+        when {
+            !grid.inside(nextPos) -> return false
+            grid[nextPos] in charArrayOf('#', 'O') -> guard.turnRight()
+            else -> guard.pos = nextPos
+        }
+
+    }
 }
 
 fun main() {
-    val inputGrid = readInput()
-    println("Part 1: ${part1(inputGrid)}")
+    val grid = readInput("input.txt")
+    val guard = grid.findGuard()
+    val wallPositions = possibleWallPositions(guard, grid)
+
+    println("Part 1: ${wallPositions.count() + 1}")
+
+    var count = 0
+    for (pos in wallPositions) {
+        val grid = grid.deepCopy()
+        grid[pos] = 'O'
+        if (hasLoop(guard, grid)) count += 1
+    }
+
+    println("Part 2: $count")
 }
